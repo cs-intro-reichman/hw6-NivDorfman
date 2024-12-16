ef8255f5833b2ba4831d70227cd9347b85758d82
NivDorfman <nivdorf@gmail.com>
2024-12-16 13:08:35 +0200

    Update Runigram.java

--- Runigram.java
+++ Runigram.java
@@ -91,9 +91,9 @@ public class Runigram {
 	public static Color[][] flippedHorizontally(Color[][] image) {
 		Color [][] newarr = new Color [image.length][image[0].length];
 		for(int i = 0; i< image.length; i++){
-			for(int j = 0; j<image[i].length;j++){
+			for(int j = 0; j<image[0].length;j++){
 
-				newarr[i][image[i].length-1-j] = image[j][i];
+				newarr[i][image[0].length-1-j] = image[i][j];
 			}
 		}
 		return newarr;
