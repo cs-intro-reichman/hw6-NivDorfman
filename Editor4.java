b68a889cefbc31c3d073d499bb04bbe033c92b96
NivDorfman <nivdorf@gmail.com>
2024-12-19 13:08:16 +0200

    test 1

--- Editor4.java
+++ Editor4.java
@@ -1,6 +1,11 @@
 import java.awt.Color;
 public class Editor4 {
     public static void main (String[] args) {
-		String source = args[0]; int num = Integer.parseInt(args[1]);	Color[][] sImgage = Runigram.read(source);	Color[][] tarImg = Runigram.grayScaled(sImgage);	Runigram.setCanvas(sImgage);Runigram.morph(sImgage, tarImg, num);
+		String source = args[0]; 
+		int num = Integer.parseInt(args[1]);	
+		Color[][] sImgage = Runigram.read(source);	
+		Color[][] tarImg = Runigram.grayScaled(sImgage);	
+		Runigram.setCanvas(sImgage);
+		Runigram.morph(sImgage, tarImg, num);
 	}
 }
